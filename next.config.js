dfa1c2d4ad28da5dc2d730b812b39476a1df88ff
凌莞 <i@gao4.pw>
2021-10-27 18:37:58 +0800

    旧博客的跳转链接

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const OLD_BLOG_TRANSFER = [
+    [43, 'sunlogin-in-opensuse'],
+    [35, 'cloud-server-freebsd'],
+    [30, 'deepin-wine-wechat-in-docker'],
+    [29, 'pitfalls-in-kvm'],
+    [28, 'server-encrypted-backup-with-rclone'],
+    [18, 'android-chroot-arch-linux'],
+    [15, 'google-sync-in-chromium'],
+    [9, 'raspi-with-ipad'],
+    [4, 'cloud-server-arch-linux'],
+]
+
 module.exports = {
     reactStrictMode: false,
     publicRuntimeConfig: {
@@ -10,6 +22,11 @@ module.exports = {
                 destination: '/favicon.webp',
                 permanent: true,
             },
+            ...OLD_BLOG_TRANSFER.map(e => ({
+                source: `/${e[0]}`,
+                destination: `/posts/${e[1]}`,
+                permanent: true,
+            })),
         ]
     },
 }
