2b3342285638e99130c0be0e3aff45e764151fdb
凌莞 <i@gao4.pw>
2021-10-27 18:24:14 +0800

    评论系统异常处理

--- components/RinMent/index.tsx
+++ components/RinMent/index.tsx
@@ -3,15 +3,28 @@ import axios from 'axios'
 import Comment from '../../types/Comment'
 import SingleComment from './SingleComment'
 import CommentBox from './CommentBox'
+import styles from '../../styles/Components.module.scss'
 
 export const BASE_URL = 'https://comments.lwqwq.com/rinment/'
 
 export default function RinMent({slug}) {
     const [comments, setComments] = useState<Comment[]>([])
+    const [error, setError] = useState('')
     useEffect(() => {
         axios.get(BASE_URL + slug)
             .then(res => setComments(res.data.data))
+            .catch(e=>{
+                console.log(e)
+                setError(e.message)
+            })
     }, [])
+
+    if(error){
+        return <div className={styles.commentBox}>
+            评论系统暂时不可用⁄(⁄ ⁄•⁄-⁄•⁄ ⁄)⁄
+            <p>{error}</p>
+        </div>
+    }
 
     return <div>
         <CommentBox slug={slug} addComment={addComment}/>
