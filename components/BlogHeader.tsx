a45df51e90478c963f069fab32436b123124d97c
Clansty <i@gao4.pw>
2022-03-02 14:58:57 +0800

    blog 界面中可以点击顶部返回

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/Components.module.scss'
 import Link from 'next/link'
 import {LeftOutlined} from '@ant-design/icons'
 
-export default function BlogHeader({postTitle}: { postTitle?: string }) {
+export default function BlogHeader({postTitle, scrollUp}: { postTitle?: string, scrollUp: () => any }) {
     return <div className={`${styles.blogHeader} blogHeader`}>
         <div className={styles.back}>
             <Link href={postTitle ? '/posts' : '/'}>
@@ -11,6 +11,6 @@ export default function BlogHeader({postTitle}: { postTitle?: string }) {
                 </a>
             </Link>
         </div>
-        <h1>{postTitle}</h1>
+        <h1 onClick={scrollUp}>{postTitle}&nbsp;</h1>
     </div>
 }
--- layouts/BlogLayout.tsx
+++ layouts/BlogLayout.tsx
@@ -4,13 +4,15 @@ import {useEffect, useRef} from 'react'
 
 export default function BlogLayout({postTitle, children}: { postTitle?: string, children: any }) {
     const contentBox = useRef<HTMLDivElement>()
-    useEffect(() => {
-        contentBox.current.scrollTo(0, 0)
-    }, [postTitle])
+    useEffect(scrollUp, [postTitle])
     return <div className={`${styles.blogLayout} ${postTitle ? styles.postLayoutContent : styles.postLayoutList}`}>
-        <BlogHeader postTitle={postTitle}/>
+        <BlogHeader postTitle={postTitle} scrollUp={scrollUp}/>
         <div className={`${styles.body} ${postTitle && 'blogBody'}`} ref={contentBox}>
             {children}
         </div>
     </div>
+
+    function scrollUp() {
+        contentBox.current.scrollTo(0, 0)
+    }
 }
